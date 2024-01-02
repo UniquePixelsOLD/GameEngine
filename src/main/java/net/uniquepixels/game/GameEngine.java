a5781a2f6803135aaa9b9f027bba80a34f5f089d
shorty <ungegrieftfirma@gmail.com>
2024-01-02 05:00:11 +0100

    removed plugin message based communication between velocity and paper

--- src/main/java/net/uniquepixels/game/GameEngine.java
+++ src/main/java/net/uniquepixels/game/GameEngine.java
@@ -1,15 +1,8 @@
 package net.uniquepixels.game;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.uniquepixels.core.paper.gui.backend.UIHolder;
-import net.uniquepixels.game.listeners.linking.DataLinker;
-import net.uniquepixels.game.maintenance.GameCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
-import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.messaging.Messenger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,39 +20,10 @@ public class GameEngine extends JavaPlugin {
 
 
     PluginManager pm = Bukkit.getPluginManager();
-
-    // DataSync load
-
-    DataLinker dataLinker = new DataLinker();
-    pm.registerEvents(dataLinker, this);
-
-    // Messaging Channels
-    Messenger messenger = Bukkit.getServer().getMessenger();
-    dataLinker.getMessageChannel().forEach((channel, pluginMessageListener) -> messenger.registerIncomingPluginChannel(this, channel, dataLinker));
-    messenger.registerOutgoingPluginChannel(this, "uniquepixels:up-send-player-to-game");
-
-    RegisteredServiceProvider<UIHolder> registration = this.getServer().getServicesManager().getRegistration(UIHolder.class);
-
-    if (registration == null) {
-
-      this.getServer().sendMessage(Component.text("Core Plugin hasn't been enabled before GameEngine start!").color(NamedTextColor.RED));
-
-      Bukkit.getPluginManager().disablePlugin(this);
-
-      return;
-    }
-
-    UIHolder uiHolder = registration.getProvider();
-
-
-    getCommand("games").setExecutor(new GameCommand(uiHolder, this));
-
-
   }
 
   @Override
   public void onDisable() {
-
 
   }
 }
